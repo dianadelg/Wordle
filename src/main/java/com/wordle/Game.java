b574e69e195fc2e0a2e34d33ab5e443a00cafe0a
dianadelg <ddelgaudio347@gmail.com>
2026-03-04 21:41:54 -0500

    fixed Game. I should not code on vacation

--- src/main/java/com/wordle/Game.java
+++ src/main/java/com/wordle/Game.java
@@ -5,16 +5,16 @@ public class Game {
 	public static void main(String[] args) {
 		System.out.println("Wordle :)");
 		System.out.println("Running on Java version: " + System.getProperty("java.version"));
-	}
 
 	// this is purely a test :) 
 
 	//System.out.println("Enter player name: ");
-	Player player = Player ("Diana");
+	Player player = new Player ("Diana");
 
 	//will need to scan file to see if prexisting user
-	System.out.print(player);
+	System.out.println(player);
 
+	}
 
 
 }
